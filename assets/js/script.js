7c578273c88e2ae735b433df089a088db1699864
Francesco <franc.ns@gmail.com>
2019-11-06 15:42:51 +0100

    added js version2 (with add in css)

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,18 @@
 
 $(document).ready(function () {
   
+  // VERSIONE 1 CON REMOVE CLASS
+  // $('.box').click(function () { 
+  //   $(this).removeClass('hidden');
+  // });
+
+  // VERSIONE 2 CON ADD IN CSS 
   $('.box').click(function () { 
-    $(this).removeClass('hidden');
+    if ($(this).hasClass('green')) {
+      $(this).css('background-color', 'green');
+    } else if ($(this).hasClass('red')) {
+      $(this).css('background-color', 'red');
+    }
   });
 
 });
